515a4bde9bdfd5caca542a684138b73708b3602d
jhmohler <jhmohler@uw.edu>
2025-03-18 21:19:39 -0700

    Changed wok to new, mesh works, drag/drop is fixed

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -10,32 +10,27 @@ public class DragObject : MonoBehaviour
 
 {
     private Vector3 offset;
-    public float y;
+    public float z;
 
     void OnMouseDown()
     {
-        // y = gameObject.transform.position.y;
+        z = gameObject.transform.position.z;
 
-        // translate pos into y-plane
-        // could change this to move along the camera ray instead of directly translating up/down
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, y, gameObject.transform.position.z);
-
-        offset = gameObject.transform.position - screenToWorld(Input.mousePosition.x, Input.mousePosition.y, y);
+        offset = gameObject.transform.position - screenToWorld(Input.mousePosition.x, Input.mousePosition.y, z);
     }
     void OnMouseDrag()
     {
-        Vector3 curPosition = screenToWorld(Input.mousePosition.x, Input.mousePosition.y, y) + offset;
+        Vector3 curPosition = screenToWorld(Input.mousePosition.x, Input.mousePosition.y, z) + offset;
         transform.position = curPosition;
     }
     // transform screen point to world point with fixed y
-    private Vector3 screenToWorld(float screen_x, float screen_y, float world_y) {
-        Plane plane = new Plane(Vector3.up, new Vector3(0f, world_y, 0f));
+    private Vector3 screenToWorld(float screen_x, float screen_y, float world_z) {
+        Plane plane = new Plane(Vector3.forward, new Vector3(0f, 0f, world_z));
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(screen_x, screen_y, 0));
         if (plane.Raycast(ray, out float distance)) {
             Vector3 worldPoint = ray.GetPoint(distance);
             return worldPoint;
         }
-        Debug.LogError("Did not intersect");
         return Vector3.zero;
     }
 }
